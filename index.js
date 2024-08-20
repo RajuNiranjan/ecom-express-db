b4d689f9a30935a017b0d918b0c7c90ced0fa9ac
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-08-20 14:31:01 +0530

    server running port 5000

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-import express from 'express';
+import express, { json } from 'express';
 import './db.js'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+
+app.use(json())
+
+app.listen(port, () => console.log(`server is running at port number : ${port}`))
