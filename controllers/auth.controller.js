302658625dc8eebc377ed578e34ee15d5f7a6cd2
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-08-21 10:10:58 +0530

    created login router

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -32,3 +32,35 @@ export const Register = async (req, res, next) => {
         res.status(500).json({ message: "Internal server error during register" })
     }
 }
+
+
+export const LogIn = async (req, res, next) => {
+    try {
+        const { email, password } = req.body
+        const user = await AuthModel.findOne({ email })
+        if (!user) {
+            return res.status(404).json({ message: "Invalid email" })
+        }
+
+        const verifyPassword = await bcrypt.compare(password, user.password)
+
+        if (!verifyPassword) {
+            return res.status(401).json({ message: "Invalid email or password" })
+        }
+
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: process.env.JWT_EXPIRES })
+
+        const userResponse = {
+            _id: user._id,
+            userName: user.userName,
+            email: user.email,
+            token: token
+        }
+
+        res.status(200).json({ message: "User login successfully", user: userResponse })
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Internal server error during login" })
+    }
+}
